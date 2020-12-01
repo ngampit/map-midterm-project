41d7e4d6e696030bdb1cd8fc3f567698bfee26da
Marcio Cruz <marciocruz@Marcios-MacBook-Pro.local>
2020-12-01 12:14:42 -0800

    create new database

--- db/schema/05_widgets.sql
+++ db/schema/05_widgets.sql
@@ -1,8 +1,40 @@
 -- Drop and recreate Widgets table (Example)
 
-DROP TABLE IF EXISTS widgets CASCADE;
-CREATE TABLE widgets (
+DROP TABLE IF EXISTS maps CASCADE;
+DROP TABLE IF EXISTS markers CASCADE;
+DROP TABLE IF EXISTS marker_icons CASCADE;
+
+CREATE TABLE maps (
   id SERIAL PRIMARY KEY NOT NULL,
   user_id INTEGER REFERENCES users(id),
-  name VARCHAR(255) NOT NULL
+  title VARCHAR(255) NOT NULL,
+  center_lat DECIMAL(17, 15) NOT NULL,
+  center_long DECIMAL(18, 15) NOT NULL,
+  is_private BOOLEAN NOT NULL DEFAULT TRUE,
+  zoom INTEGER NOT NULL DEFAULT 12,
+  search_keyword VARCHAR(255),
+  created_at TIMESTAMP,
+  updated_at TIMESTAMP,
+  description TEXT
 );
+
+CREATE TABLE marker_icons (
+  id SERIAL PRIMARY KEY NOT NULL,
+  name VARCHAR(255),
+  description TEXT
+);
+
+CREATE TABLE markers (
+  id SERIAL PRIMARY KEY NOT NULL,
+  user_id INTEGER REFERENCES users(id),
+  map_id INTEGER REFERENCES maps(id),
+  title VARCHAR(255) NOT NULL,
+  lat DECIMAL(17, 15) NOT NULL,
+  long DECIMAL(18, 15) NOT NULL,
+  created_at TIMESTAMP,
+  updated_at TIMESTAMP,
+  description TEXT,
+  icon_id INTEGER REFERENCES marker_icons(id),
+  image VARCHAR(255)
+);
+
--- db/seeds/01_users.sql
+++ db/seeds/01_users.sql
@@ -1,3 +1,7 @@
 -- Users table seeds here (Example)
-INSERT INTO users (name) VALUES ('Alice');
-INSERT INTO users (name) VALUES ('Kira');
+INSERT INTO users (name, email, password, created_at) VALUES ('Alice', 'alice@gmail.com', '1234', NOW());
+INSERT INTO users (name, email, password, created_at) VALUES ('Kira', 'kira@hotmail.com', '1234', NOW());
+INSERT INTO users (name, email, password, created_at) VALUES ('George', 'george@hotmail.com', '1234', NOW());
+INSERT INTO users (name, email, password, created_at) VALUES ('John', 'john@hotmail.com', '1234', NOW());
+
+
